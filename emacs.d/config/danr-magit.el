45b88191cb056349c2e29ff4b057a0f10c24e23a
Dan Rosén <danr@student.chalmers.se>
2012-11-06 14:17:04 +0100

    Support entering visual mode and stage hunks in magit

--- emacs.d/config/danr-magit.el
+++ emacs.d/config/danr-magit.el
@@ -14,13 +14,18 @@
         (define-key magit-mode-map (kbd "T") 'magit-key-mode-popup-tagging)
         (define-key magit-mode-map (kbd "h") 'magit-toggle-diff-refine-hunk)
         (define-key magit-mode-map (kbd "s") 'magit-stage-item)
+        (define-key magit-mode-map (kbd "V") 'evil-visual-line)
         (define-key magit-mode-map (kbd "j") (lookup-key evil-motion-state-map "j"))
         (define-key magit-mode-map (kbd "g") (lookup-key evil-motion-state-map "g"))
         (define-key magit-mode-map (kbd "<tab>") (lookup-key evil-motion-state-map [tab]))
         (define-key magit-mode-map (kbd ";") (lookup-key magit-mode-map ":"))))
 
+(evil-define-key 'visual magit-mode-map
+  [escape] 'evil-emacs-state
+  "s"      'magit-stage-item)
+
 ;; in commit edit mode, override g# and define jc
-(evil-set-initial-state 'magit-log-edit-mode 'normal)
+(evil-set-initial-state 'magit-log-edit-mode 'insert)
 (evil-define-key 'normal magit-log-edit-mode-map
   "g#" 'magit-log-edit-commit
   "jc" 'magit-log-edit-cancel-log-message)
